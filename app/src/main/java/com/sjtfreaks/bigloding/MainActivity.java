b97725425cb2789e8be5d14003e48424d23c6218
sjtfreaks <13028911306@163.com>
2018-11-14 17:16:39 +0800

    百度地图定位Sdk引入与封装 2018.11.14 17：15
    完成部分二维码开发

--- app/src/main/java/com/sjtfreaks/bigloding/MainActivity.java
+++ app/src/main/java/com/sjtfreaks/bigloding/MainActivity.java
@@ -1,13 +1,85 @@
 package com.sjtfreaks.bigloding;
 
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.sjtfreaks.bigloding.adapter.ViewPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView bottomNavigationView;
+    private ViewPager mViewPager;
+    private ViewPagerAdapter viewPagerAdapter;
+    private MenuItem menuItem;
+    private List<Fragment> mFragment;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
     }
+
+    private void initView() {
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        BottomNavigationViewHelper.disableShiftMode(bottomNavigationView);
+        mViewPager = (ViewPager) findViewById(R.id.vp);
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener(){
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+            @Override
+            public void onPageSelected(int position) {
+                if (menuItem != null) {
+                    menuItem.setChecked(false);
+                } else {
+                    bottomNavigationView.getMenu().getItem(0).setChecked(false);
+                }
+                menuItem = bottomNavigationView.getMenu().getItem(position);
+                menuItem.setChecked(true);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+        viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        mViewPager.setAdapter(viewPagerAdapter);
+        mFragment = new ArrayList<>();
+        mFragment.add(OneFragment.newInstance("首页"));
+        mFragment.add(TwoFragment.newInstance("考点"));
+        mFragment.add(ThreeFragment.newInstance("例题"));
+        mFragment.add(FourFragment.newInstance("个人"));
+        viewPagerAdapter.setList(mFragment);
+    }
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(MenuItem item) {
+            menuItem = item;
+            switch (item.getItemId()) {
+                case R.id.ic_home:
+                    mViewPager.setCurrentItem(0);
+                    return true;
+                case R.id.ic_one:
+                    mViewPager.setCurrentItem(1);
+                    return true;
+                case R.id.ic_two:
+                    mViewPager.setCurrentItem(2);
+                    return true;
+                case R.id.ic_user:
+                    mViewPager.setCurrentItem(3);
+                    return true;
+            }
+            return false;
+        }
+    };
 }
